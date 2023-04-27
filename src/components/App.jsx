bc641a7c657ab8bbc97f5e7ffde34a34747a4007
InnaShavarovska <innashavarovska@gmail.com>
2023-04-27 14:43:45 +0300

    +profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import { Profile } from "./Profile/Profile";
+
+
+
+import user from './/Data/user.json'
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +18,14 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <div> <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+      </div>
     </div>
   );
 };
