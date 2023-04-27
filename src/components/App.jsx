8b6e01dc39e86955e161b517d12d5697f8a2257a
InnaShavarovska <innashavarovska@gmail.com>
2023-04-27 18:44:59 +0300

    +friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,33 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from "./Friends/FriendList";
 
 
 
-import user from './/Data/user.json'
-
-
+import user from './/Data/user.json';
+import data from './Data/data.json';
+import friends from './Data/friends.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
+      <div style={{
+        height: 'auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      <div> <Profile
+      }}>
+        <div> <Profile
     username={user.username}
     tag={user.tag}
     location={user.location}
     avatar={user.avatar}
     stats={user.stats}
   />
-      </div>
+  <Statistics title="Upload stats" stats={data}/>
+  <FriendList friends={friends}/>
+    </div>
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,47 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const createColor = () => {
+	const color =
+	  'rgba(' +
+	  Math.round(Math.random() * 255) +
+	  ',' +
+	  Math.round(Math.random() * 255) +
+	  ',' +
+	  Math.round(Math.random() * 255) +
+	  ',' +
+	  0.8 +
+	  ')';
+ 
+	return color;
+ };
+
+export const Statistics = ({title, stats}) => {
+	return (
+			<section className={css.statistics}>
+	{title && <h2 className={css.title}>{title}</h2>}
+	
+	  <ul className={css.stat__list}>
+	  {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.stats}
+              key={id}
+              style={{ backgroundColor: createColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+	  </ul>
+	</section>
+	);
+};
+
+Statistics.propTypes = {
+	title: PropTypes.string.isRequired,
+	stats: PropTypes.array.isRequired,
+ };
+
+
